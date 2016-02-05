12825896fa1423b037ac240b325abccc5577a85b
virtualedd <virtualeddsystem@gmail.com>
2016-02-05 14:34:33 -0300

    definición de struct palabra

--- socialcrawler.cpp
+++ socialcrawler.cpp
@@ -2,6 +2,16 @@
 #include <fstream>
 using namespace std;
 
+//Nodo de una lista enlazada de palabras
+struct palabra
+{
+    char palabra[30];
+    int frq;
+    palabra* sig;
+}
+
+// -- MAIN --
+
 int main()
 {
     return 0;
