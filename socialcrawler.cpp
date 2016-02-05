b7c5751a78dde9603532f796588dc7b6ef6cf4d3
virtualedd <virtualeddsystem@gmail.com>
2016-02-05 14:48:42 -0300

    definición de PalabraList: función para iniciar una nueva lista de palabras

--- socialcrawler.cpp
+++ socialcrawler.cpp
@@ -1,18 +1,33 @@
 #include <iostream>
 #include <fstream>
+#include <string.h>
+
 using namespace std;
 
 //Nodo de una lista enlazada de palabras
 struct palabra
 {
-    char palabra[30];
+    char pal[30];
     int frq;
-    palabra* sig;
+    palabra* sig = NULL;
+}
+
+//Función para definir una nueva lista de palabras
+//retorna un puntero al primer elemento
+
+palabra* PalabraList(char valor[30])
+{
+    palabra* inicio = new palabra();
+    inicio -> pal = valor;
+    return  inicio;
 }
 
 // -- MAIN --
 
 int main()
 {
+    ifstream archivo('ruta.txt', ios::binary);
+    
+    
     return 0;
 }
